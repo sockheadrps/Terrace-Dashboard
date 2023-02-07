3d2ce370ef64faee4ae752d916b56386b7a9322e
Beaux Moreau <beaux7312@gmail.com>
2023-02-07 11:58:30 -0600

    Basically finished the sveltekit version

---
+++ Terrace-kit/src/lib/bookmarksStore.js
@@ -0,0 +1,48 @@
+import { writable } from "svelte/store";
+
+let bookmarkStorage = [
+    {name: "Youtube", url: "https://www.youtube.com", "icon": "logos:youtube-icon"},
+    {name: "Reddit", url: "https://www.reddit.com", "icon": "logos:reddit-icon"},
+    {name: "Twitch", url: "https://www.twitch.com", "icon": "logos:twitch"},
+    {name: "Instagram", url: "https://www.instagram.com", "icon": "skill-icons:instagram"},
+]
+
+
+export function loadBookmarks() {
+    if(localStorage.getItem("bookmarks") === null) {
+        localStorage.setItem("bookmarks", JSON.stringify(bookmarkStorage))
+    } else {
+        console.log('doodly')
+        bookmarkStorage = JSON.parse(localStorage.getItem("bookmarks"))
+    }
+}
+
+export function newBookmark(newBookmark) {
+    let index = bookmarkStorage.findIndex((n) => {
+         return newBookmark.name === n.name
+    })
+    if (index > -1) {
+        bookmarkStorage[index] = newBookmark
+    } else {
+        bookmarkStorage.push(newBookmark)
+    }
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarkStorage))
+    bookmarkList.set(bookmarkStorage)
+}
+
+export function deleteBookmark(name) {
+    bookmarkStorage = JSON.parse(localStorage.getItem("bookmarks"))
+    let toDelete = bookmarkStorage.filter(entry => entry.name === name)[0]
+    if (toDelete) {
+        let filteredBookmarks = bookmarkStorage.filter(entry => entry.name !== name)
+        localStorage.setItem("bookmarks", JSON.stringify(filteredBookmarks))
+        bookmarkList.set(filteredBookmarks)
+        return toDelete
+    }
+}
+
+export function saveBookmarks() {
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarkStorage));
+}
+
+export let bookmarkList = writable(bookmarkStorage)
