c1a129027c1e781d057dc0a915081b365991c2dd
sockheadrps <r.p.skiles@gmail.com>
2023-02-07 06:36:53 -0500

    Initial commit building environment
    Using TS, Tailwind and sveltekit

---
+++ Terrace-kit/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  
+  <style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.100);
+    }
+  </style>
